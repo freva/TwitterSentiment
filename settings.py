1437b65a0cc56fa1304d198df85f35d571a26bfa
Oyvind M <moldestad@gmail.com>
2015-02-04 19:40:53 +0100

    Updated settings

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'f)!nw+q&3@^28c5lj8h+9*cn%hj%(143@*mj5l21iebgl2rpg2'
 
 if '3883' in socket.gethostname():
-    }    DATABASES = {
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': '/root/TwitterSentiment/db.sqlite3',
@@ -26,6 +26,7 @@ else:
             'HOST': '',
             'PORT': '',
         }
+    }
 
 INSTALLED_APPS = (
     	'data',
