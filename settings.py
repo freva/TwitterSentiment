89d195a597b76f8cd2fb13812ea2393baf5a5050
Oyvind M <moldestad@gmail.com>
2015-02-04 19:39:05 +0100

    Updated db based on host

--- settings.py
+++ settings.py
@@ -1,19 +1,31 @@
 import os
+import socket
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'f)!nw+q&3@^28c5lj8h+9*cn%hj%(143@*mj5l21iebgl2rpg2'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+if '3883' in socket.gethostname():
+    }    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': '/root/TwitterSentiment/db.sqlite3',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
 
 INSTALLED_APPS = (
     	'data',
