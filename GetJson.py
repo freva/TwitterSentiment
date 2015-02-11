b78dc7d2c925f4cc2cb094a494c55aa72783f8fe
Oyvind M <moldestad@gmail.com>
2015-02-11 12:20:57 +0100

    aosidn

--- GetJson.py
+++ GetJson.py
@@ -1,6 +1,7 @@
 import db
 from data.models import Tweet
 import json
+import socket
 from django.db.models import Q
 
 def get_tweets():
@@ -20,8 +21,12 @@ def main():
 	for tweet in get_tweets():
 		dictionary.append({"lat":float(tweet.lat), "lng":float(tweet.lng), "polarity":float(tweet.polarity)})
 
-	with open("map.json", "w+") as output_file:
-		json.dump(dictionary, output_file)
+	if '3883' in socket.gethostname():
+		with open("/root/TwitterSentiment/map.json", "w+") as output_file:
+			json.dump(dictionary, output_file)
+	else:
+		with open("map.json", "w+") as output_file:
+			json.dump(dictionary, output_file)
 
 if __name__ == "__main__":
 	main()
