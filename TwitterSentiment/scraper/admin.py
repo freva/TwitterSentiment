1817081488efe143560ddb8df7d87fee1d7373d0
Oyvind M <moldestad@gmail.com>
2015-02-18 12:34:53 +0100

    Added admin

--- TwitterSentiment/scraper/admin.py
+++ TwitterSentiment/scraper/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TweetAdmin(admin.ModelAdmin):
+	list_display = ('hashtag', 'city', 'state', 'polarity')
+	list_filter = ('hashtag',)
+
+admin.site.register(Tweet, TweetAdmin)
