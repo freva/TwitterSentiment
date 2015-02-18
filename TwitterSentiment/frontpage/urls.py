b6d4e4c189572dd6796aa8c07e9b2c6054e0ec01
Limon <valerijf@stud.ntnu.no>
2015-02-18 12:05:01 +0100

    Added reference to static folder

--- TwitterSentiment/frontpage/urls.py
+++ TwitterSentiment/frontpage/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, url
+from django.conf import settings
 
 urlpatterns = patterns('TwitterSentiment.frontpage.views',
 	url(r'^$', 'home', name='home'),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
