b4fae34871d32f8fe14c20915efd98231e788bb1
Limon <valerijf@stud.ntnu.no>
2015-02-25 11:52:01 +0100

    Changed bubble color palette

--- TwitterSentiment/frontpage/JsonConverter.py
+++ TwitterSentiment/frontpage/JsonConverter.py
@@ -38,7 +38,8 @@ class JsonConverter(object):
                       for hashtag in hashtags
                       for t in Tweet.objects.filter(hashtag= hashtag)]
 
-        results = JsonConverter.doCluster([t for t in dictionary if t["polarity"][0] < 0]) + JsonConverter.doCluster([t for t in dictionary if t["polarity"][0] >= 0])
+        #results = JsonConverter.doCluster([t for t in dictionary if t["polarity"][0] < 0]) + JsonConverter.doCluster([t for t in dictionary if t["polarity"][0] >= 0])
+        results = JsonConverter.doCluster(dictionary)
         results = [{"lat": t["lat"], "lng": t["lng"], "count": len(t["polarity"]),
                     "polarity": sum(t["polarity"])/len(t["polarity"]),
                     "variance": sum([i*i for i in t["polarity"]])/len(t["polarity"])}
