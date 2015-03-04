5d4bafb297ff0a3f0a0e9cef9dc9f95cc0f433a4
Limon <valerijf@stud.ntnu.no>
2015-03-04 19:31:55 +0100

    Fixed placeholdertext

--- TwitterSentiment/static/js/jquery.tokeninput.js
+++ TwitterSentiment/static/js/jquery.tokeninput.js
@@ -28,6 +28,7 @@ var DEFAULT_SETTINGS = {
     hintText: "Type in a search term",
     noResultsText: "No results",
     searchingText: "Searching...",
+    placeholderText: "",
     deleteText: "&times;",
     animateDropdown: true,
     theme: null,
@@ -235,7 +236,7 @@ $.TokenList = function (input, url_or_data, settings) {
     var input_val;
 
     // Create a new text input an attach keyup events
-    var input_box = $("<input type=\"text\"  autocomplete=\"off\">")
+    var input_box = $('<input type="text"  autocomplete="off" placeholder="' + $(input).data("settings").placeholderText + '">')
         .css({
             outline: "none"
         })
