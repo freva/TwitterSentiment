9274cc946f9b07938f5c9dbf1bacc9bf98cf75a5
Oyvind M <moldestad@gmail.com>
2015-03-04 12:34:29 +0100

    Added overviews

--- TwitterSentiment/frontpage/urls.py
+++ TwitterSentiment/frontpage/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 urlpatterns = patterns('TwitterSentiment.frontpage.views',
 	url(r'^$', 'home', name='home'),
 	url(r'^get_hashtag/$', 'get_hashtag', name='get_hashtag'),
+	url(r'^cases/$', 'cases', name='cases'),
+	url(r'^hashtags/$', 'hashtags', name='hashtags'),
 )
 
 urlpatterns += patterns('',
--- TwitterSentiment/frontpage/views.py
+++ TwitterSentiment/frontpage/views.py
@@ -1,10 +1,11 @@
 from TwitterSentiment.scraper.management.commands import load_cases
 from django_ajax.decorators import ajax
 from django.shortcuts import render
-from TwitterSentiment.scraper.models import Tweet
+from TwitterSentiment.scraper.models import Tweet, Tag, Case
 import json
 from django.http import HttpResponse
 from .aggregate_tweets import JsonConverter
+from .stats import Cases, Hashtags
 
 
 def home(request):
@@ -17,6 +18,25 @@ def home(request):
     return render(request, 'base.html',
         {'tags': json.dumps(searchTerms)})
 
+def cases(request):
+	cases = [Cases(c) for c in Case.objects.all().order_by('name')]
+	hashtags_from_cases = sum([c.hashtags for c in cases])
+	tweets_from_cases = sum([c.tweets for c in cases])
+	return render(request, 'cases.html',
+		{'cases':[Cases(c) for c in Case.objects.all().order_by('name')],
+		'hashtags_from_cases':hashtags_from_cases,
+		'tweets_from_cases':tweets_from_cases,
+		'hashtags_from_database':Tag.objects.count(),
+		'tweets_from_database':Tweet.objects.count()})
+
+def hashtags(request):
+	hashtags = [Hashtags(t) for t in Tag.objects.all().order_by('name')]
+	tags = len(hashtags)
+	tweets = sum([h.tweets for h in hashtags])
+	return render(request, 'hashtags.html',
+		{'hashtags':hashtags,
+		'tags':tags,
+		'tweets':tweets})
 
 @ajax
 def get_hashtag(request):
