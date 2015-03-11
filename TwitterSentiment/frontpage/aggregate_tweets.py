0498052ca20bfc84b1c331d4bf01bef96749dc97
Limon <valerijf@stud.ntnu.no>
2015-03-11 14:29:45 +0100

    Added id selection

--- TwitterSentiment/frontpage/aggregate_tweets.py
+++ TwitterSentiment/frontpage/aggregate_tweets.py
@@ -1,5 +1,6 @@
 from TwitterSentiment.scraper.models import Tweet
 from math import sqrt, pow, log
+import random
 
 class JsonConverter(object):
     @staticmethod
@@ -29,27 +30,30 @@ class JsonConverter(object):
     @staticmethod
     def overlap(obj1, obj2):
         dist = sqrt(pow(obj1["lat"]-obj2["lat"], 2) + pow(obj1["lng"]-obj2["lng"], 2))
-        return dist - 0.25*(log(len(obj1["polarity"])+2) + log(len(obj2["polarity"])+2)) < 0
+        return dist - 0.25*(log(len(obj1["polarity"][0])+2) + log(len(obj2["polarity"][0])+2)) < 0
 
 
     @staticmethod
     def searchHashtags(hashtags, startTime, endTime):
-        dictionary = [{"lat": float(t.lat), "lng": float(t.lng), "polarity": [float(t.polarity)]}
+        dictionary = [{"lat": float(t.lat), "lng": float(t.lng), "polarity": [(float(t.polarity), t.tweet_id)]}
                       for hashtag in hashtags
                       for t in Tweet.objects.filter(hashtag= hashtag, created_at__gt=startTime, created_at__lt=endTime)]
 
         results =  JsonConverter.doCluster(dictionary)
         for t in results:
-            groups = [0.1, 0.1, 0.1]
-            for pol in t["polarity"]:
+            groups = [[], [], []]
+            random.shuffle(t["polarity"])
+            for pol, tweetID in t["polarity"]:
                 if pol < -0.2:
-                    groups[0] += 1
+                    groups[0].append(tweetID)
                 elif pol < 0.2:
-                    groups[1] += 1
+                    groups[1].append(tweetID)
                 else:
-                    groups[2] += 1
+                    groups[2].append(tweetID)
+
             t["count"] = len(t["polarity"])
-            t["polarity"] = groups
+            t["polarity"] = [0.1+len(group) for group in groups]
+            t["ids"] = [group[:5] for group in groups]
 
         return results
 
