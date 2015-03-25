182d25f853fc176cd9d2b41815a9ed22bbb273d1
Limon <valerijf@stud.ntnu.no>
2015-03-25 10:18:03 +0100

    Changed from search_hashtags to get_tokens

--- TwitterSentiment/frontpage/urls.py
+++ TwitterSentiment/frontpage/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('TwitterSentiment.frontpage.views',
 	url(r'^$', 'home', name='home'),
 	url(r'^get_hashtag/$', 'get_hashtag', name='get_hashtag'),
-    url(r'^get_tokens/$', 'search_hashtag', name='search_hashtag'),
+    url(r'^get_tokens/$', 'get_tokens', name='get_tokens'),
     url(r'^graph/$', 'graph_hashtag', name='graph_hashtag'),
 )
