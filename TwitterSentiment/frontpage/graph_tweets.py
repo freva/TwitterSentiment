31037e717492fe2bd03d36b9b7e37f9ef9581ae7
Limon <valerijf@stud.ntnu.no>
2015-03-25 15:23:35 +0100

    Added confidence interval

--- TwitterSentiment/frontpage/graph_tweets.py
+++ TwitterSentiment/frontpage/graph_tweets.py
@@ -26,9 +26,14 @@ def graphHashtags(hashtags, startTime, endTime):
         polar[index if index < numBins else -1].append(tweet["polarity"])
 
 
+    averages = [sum(pol)/len(pol) if len(pol)>0 else 0 for pol in polar]
+    sumOfSquares = [sum([pow(i-averages[avgIndex], 2)/len(polar[avgIndex])]) for avgIndex in xrange(len(averages)) for i in polar[avgIndex]]
+    confIntSize = [1.96*pow(sumOfSquares[i]/len(polar[i]), 0.5) for i in xrange(len(sumOfSquares))]
+
     xAxis = {"categories": labels, "labels": {"format": '{value:%e %b %H:%M}'}}
-    yAxis = [{"name": "Polarity", "type": "spline", "data": [sum(pol)/len(pol) if len(pol)>0 else 0 for pol in polar]},
-           {"name": "Frequency", "type": "column", "yAxis": 1, "data": [len(pol) for pol in polar]}]
+    yAxis = [{"name": "Polarity", "type": "spline", "data": averages},
+        {"name": "Frequency", "type": "column", "yAxis": 1, "data": [len(pol) for pol in polar]},
+        {"name": "Conf. int.", "type": "arearange", "yAxis": 2, "data": [[averages[i]-confIntSize[i], averages[i]+confIntSize[i]] for i in xrange(len(averages))]}]
 
     return {"xAxis": xAxis, "yAxis": yAxis}
 
