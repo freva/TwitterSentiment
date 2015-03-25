5b361435db87a1db5a430e615c657ef239ca574a
Limon <valerijf@stud.ntnu.no>
2015-03-25 09:41:05 +0100

    Searching tags through GET

--- TwitterSentiment/frontpage/views.py
+++ TwitterSentiment/frontpage/views.py
@@ -2,20 +2,12 @@ from TwitterSentiment.scraper.management.commands import load_cases
 from django_ajax.decorators import ajax
 from django.shortcuts import render
 from TwitterSentiment.scraper.models import Tweet, Tag, Case
-import json
 import datetime
 from django.http import HttpResponse
 from .aggregate_tweets import JsonConverter
 
 def home(request):
-    searchTerms = [{"case": label, "name": label, "id": ','.join(load_cases.CASES[label])}
-                   for label in load_cases.CASES.keys()]
-
-    searchTerms += [{"case": label, "name": "#" + hashtag, "id": hashtag}
-                    for label in load_cases.CASES.keys() for hashtag in load_cases.CASES[label]]
-
-    return render(request, 'base.html',
-        {'tags': json.dumps(searchTerms)})
+    return render(request, 'base.html')
 
 @ajax
 def get_hashtag(request):
@@ -27,17 +19,15 @@ def get_hashtag(request):
     return {'results': results}
 
 
-@ajax
 def search_hashtag(request):
     query = request.GET.get('q', '')
 
     out = [{"name": label, "id": ','.join(load_cases.CASES[label])} for label in load_cases.CASES.keys() if query in label]
     out += [{"name": "#" + hashtag, "id": hashtag}
                     for label in load_cases.CASES.keys() for hashtag in load_cases.CASES[label] if query in hashtag]
-    return [query, out]
+    return out
 
 
-@ajax
 def graph_hashtag(request):
     hashtags = list(set(request.GET.get('hashtag', '').split(",")))
     startTime = datetime.datetime.strptime(request.GET.get('from', ''), "%d/%m/%Y %H:%M")
